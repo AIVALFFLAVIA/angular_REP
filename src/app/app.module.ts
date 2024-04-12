f27801668320d9a30a0f3734dfc9362252f1d317
Flavia Rira <flaviarira23@gmail.com>
2024-04-12 11:12:04 +0200

    Initial commit for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { PropertyDetailsComponent } from './components/property-details/property-details.component';
+import { PropertyPostComponent } from './components/property-post/property-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SearchComponent,
+    PropertyDetailsComponent,
+    PropertyPostComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/property-post/property-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-post',
+  templateUrl: './property-post.component.html',
+  styleUrl: './property-post.component.css'
+})
+export class PropertyPostComponent {
+
+}
