b48472161f64abd24ee35c7b06aff2a306607a5f
fabianlika <fabjan.lika@fti.edu.al>
2024-04-15 21:17:10 +0200

    Add admin component

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrls: ['./admin.component.css'] 
 })
 export class AdminComponent {
 
+  constructor(private router: Router) { }
+
+  navigateToPropertyManagement(): void {
+    this.router.navigate(['/property-management']);
+  }
+
+  navigateToUserManagement(): void {
+    this.router.navigate(['/user-management']);
+  }
 }
